a2b566caf1cf0ea0e2ffd092851dc2cbfb4c1f1e
eggix <tobieckert@live.de>
2014-11-10 21:18:43 +0100

    Update iss.m

--- iss.m
+++ iss.m
@@ -4,14 +4,14 @@ h = 400*10^3;
 E = 6371*10^3;
 m = 5.9736*10^24;
 G = 6.67384*10^(-11);
-s = 7770;	%initial speed
-dt = 0.1;
-n = 10000;	%number of steps, calulation could take a while for n>1000
+s = 7770;		%initial speed
+dt = 0.1;		%timestep
+n = 10000;		%number of steps, calulation could take a while for n>1000
 
 
 r = [E+h;0]
 v = [0;s]
-l = norm (r)	%length of vector r
+l = norm (r)		%length of vector r
 a = -G*m/(l^2)*r/l 	%r/l is vector of length 1 and direction of vector r
 x = r(1);
 y = r(2);
